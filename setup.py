3593d73254e328e241c035090916791c01f3c359
Randy C. Wihandika <rendicahya@gmail.com>
2022-03-25 21:01:16 +0700

    Getting domain from the YAML config file, incrementing version.

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 import setuptools
 
+from config import get_config
+
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+domain = get_config()['domain']
+
 setuptools.setup(
     name='sinta-scraper',
-    version='0.13.1',
+    version='0.14.0',
     author='Randy Cahya Wihandika',
     author_email='rendicahya@gmail.com',
-    description='Retrieves information from Sinta (http://sinta.ristekbrin.go.id) via scraping.',
+    description=f'Retrieves information from Sinta ({domain}) via scraping.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/rendicahya/sinta-scraper',
     packages=setuptools.find_packages(),
-    install_requires=['bs4', 'requests', 'dicttoxml', 'dict2xml', 'python-string-utils'],
+    install_requires=['bs4', 'requests', 'dicttoxml', 'dict2xml', 'python-string-utils', 'yaml-helper'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
