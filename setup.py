e641b4b5c40abf9e6b2604bc0cdee921ddb34f21
Randy C. Wihandika <rendicahya@gmail.com>
2022-03-28 09:02:53 +0700

    Incrementing version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='sinta-scraper',
-    version='0.14.5',
+    version='0.14.6',
     author='Randy Cahya Wihandika',
     author_email='rendicahya@gmail.com',
     description='Retrieves information from Sinta (https://sinta.kemdikbud.go.id) via scraping.',
